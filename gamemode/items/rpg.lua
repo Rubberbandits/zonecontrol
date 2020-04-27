d0a3c9261e9d2671cb9a6aafd702f4a39f411d70
rusty <dalegribble@riseup.net>
2020-04-27 02:42:24 -0700

    Removed second RPG item Was totally breaking the script.

--- gamemode/items/rpg.lua
+++
@@ -1,9 +0,0 @@
-
-ITEM.Name =  "OG-7V";
-ITEM.Desc =  "A high-explosive rocket used for the RPG-7.";
-ITEM.Model =  "models/stalker/ammo/rpg7.mdl";
-ITEM.Weight =  2;
-ITEM.FOV =  18;
-ITEM.CamPos =  Vector( 50, 50, 50 );
-ITEM.LookAt =  Vector( 0, 0, 0 );
-ITEM.BulkPrice =  10000;
