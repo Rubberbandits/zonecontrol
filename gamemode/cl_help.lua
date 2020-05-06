61ce3ea7493c31996cab9b03b46aeba4950f4801
rusty <dalegribble@riseup.net>
2020-05-06 08:20:03 -0700

    Help menu updates

--- gamemode/cl_help.lua
+++ gamemode/cl_help.lua
@@ -11,40 +11,77 @@ function GM:RefreshHelpMenuContent()
 		},
 		{ "Chat",
 			[[Just entering something in the chatbox will make you say it in character.
-			
-			/help - Show this menu
 
-			/y - Yell
-			/w - Whisper
-			/me - Action
-			/it - World action
-			// - Global OOC
-			[[, .// - Local OOC
-			/a - Talk to admins
-			/pm [name] [text] - PM another player
-			/r - Talk on your radio if you have one
-			/help - Open this menu
-			
-			/rus - Speak Russian.
-			/chi - Speak Chinese.
-			/jap - Speak Japanese.
-			/spa - Speak Spanish.
-			/fre - Speak French.
-			/ger - Speak German.
-			/ita - Speak Italian. ]] },
+            /y - Yell
+            /w - Whisper
+            /me - Action
+            /it - World action
+            /ev - Emotes world actions globally, for GM's and Admins
+            /lev - Emotes world actions locally, for GM's and Admins
+
+            /mask - Remove your mask or helmet, if you have a suit that also has a mask or helmet.
+
+            /pda all - Send a public message to all PDA users
+            /pda username - Send a private message to a specific PDA username
+
+            /roll 1d20 - Roll one, twenty sided die.
+            /roll nds - Without changing variable d, replace n for number of dice and s for number of sides.
+
+            // - Global OOC
+            [[, .// - Local OOC
+            /a - Talk to admins
+            /pm [name] [text] - PM another player
+            /r - Talk on your radio if you have one
+
+            /eng - Speak English.
+            /pol - Speak Polish.
+            /chi - Speak Chinese.
+            /jap - Speak Japanese.
+            /spa - Speak Spanish.
+            /fre - Speak French.
+            /ger - Speak German.
+            /ita - Speak Italian.]] },
 		{ "Binds", [[F1 - Open help menu.
 		F2 - Open character menu.
 		F3 - Open player menu.
 		F4 - Open admin menu.
 		C - Open context menu.]] },
-		{ "Story", [[ TBD ]] },
+		{ "Using a PDA",
+		[[PDA's are an item you must acquire in order to use the Stalker.net service.
+		Register your username if you want to be able to message people privately. If your PDA is annoying you, turn it off.
+
+		/pda all - Send a public message to all PDA users
+		/pda username - Send a private message to a specific PDA username. Use the Contacts tab to find active users
+
+		When viewing the contents of your PDA, you should find the Journal, Contacts, and Messages tab, as well as a clock
+
+		The Journal allows you to take personal notes. Tap the green New entry button to create a new entry
+		Select a title for your entry, then fill out the contents. Handy for writing poems during your roadside picnic
+
+		The Contacts tab allows you to view all other active PDA users connected to your network
+
+		The Messages tab will log and date messages you send and receive
+
+		Extra functions and updates to your PDA system may come overtime! Check back here to learn new functions]] 
+		},
 		{ "Tooltrust", [[By default, you don't have tooltrust, you have phystrust, and you have proptrust. Phys- and proptrust give you a physgun and the ability to spawn props respectively. Tooltrust gives you a toolgun.
 		
 		Basic tooltrust gives you some common simple tools, a slightly increased prop limit, and slightly increased prop spawn permissions. To get this, ask an admin.
 		
 		Advanced tooltrust gives you most tools, an increased prop limit, and increased prop spawn permissions. Advanced tooltrust users' props are solid, whereas basic and non tooltrusted props are no-collided. To get this, check the forums.
 		
-		Admins can take away phys and proptrust if you abuse the privillege - you can get it back on the forums.]] },
+		Admins can take away phys and proptrust if you abuse the privillege - you can get it back on the forums.]] 
+		},
+		{ "Flags", 
+			[[ZoneControl uses a flag system. Most flags are automatic - you don't need to run a command to access them.
+			The trader flag will soon be divided into other flags.
+
+			Character Flags
+			T - Technician
+			X - Black Market Dealer
+			G - Gamemaster, per-character
+			]] 
+		},
 	};
 
 	if( LocalPlayer():IsAdmin() ) then
