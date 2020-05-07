f6afc57c8d307e321f232790e1f7988c43449554
rusty <dalegribble@riseup.net>
2020-05-07 14:07:51 -0700

    added fire extinguisher

---
+++ gamemode/items/weapon/weapon_srp_fireextinguisher.lua
@@ -0,0 +1,15 @@
+ITEM.Name = "Fire Extinguisher"
+ITEM.Desc = "Used for extinguishing fires. Also pretty weighty."
+ITEM.Model = "models/weapons/w_fire_extinguisher.mdl"
+ITEM.WeaponClass = "weapon_extinguisher"
+ITEM.Weight = 5
+ITEM.SafetyPos = Vector(0,0,0)
+ITEM.SafetyAng = Angle(-12.4, 0, 0)
+ITEM.DegradeRate = 0.05
+ITEM.FOV             = 46
+ITEM.CamPos         = Vector( 50, 4.47, 0.9 )
+ITEM.LookAt         = Vector( -90, -4.82, 4.83 )
+ITEM.SelfRepairCondition = 100
+ITEM.License = M;
+ITEM.BulkPrice = 10000
+ITEM.Slot = 2
