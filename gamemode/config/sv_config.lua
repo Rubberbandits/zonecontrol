e05587eeaff4774cec17bb9e59815fe6d0db8a90
Rubberbandits <Rubberbandits@users.noreply.github.com>
2021-03-20 14:53:46 -0700

    change steamapi key location for security reasons

--- gamemode/config/sv_config.lua
+++ gamemode/config/sv_config.lua
@@ -3,3 +3,6 @@ GM.MySQLUser = "root";
 GM.MySQLPass = "";
 GM.MySQLDB = "test";
 GM.MySQLPort = 3306;
+
+-- we store this in a file that is usually ignored for security, so its not in a repo
+GM.SteamAPIKey = "";
