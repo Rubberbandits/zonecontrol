14b24917c032551dad6c078ab8f6536c7e80506e
rusty <dalegribble@riseup.net>
2021-03-23 13:47:31 -0700

    modify global jam chance

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -136,7 +136,7 @@ function GM:Initialize()
 	game.ConsoleCommand( "sv_tfa_cmenu 0\n" );
 	game.ConsoleCommand( "sv_tfa_default_clip 0\n" );
 	game.ConsoleCommand( "sv_tfa_jamming_factor_inc 0.5\n" );
-	game.ConsoleCommand( "sv_tfa_jamming_mult 0.8\n" );
+	game.ConsoleCommand( "sv_tfa_jamming_mult 0.75\n" );
 	game.ConsoleCommand( "sv_tfa_penetration_hitmarker 0\n" );
 	
 	if( game.IsDedicated() and !self.PrivateMode ) then
