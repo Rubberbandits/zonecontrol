b69136bc7be93b824b9d337a75e1b92f74c28b97
server <root@server>
2021-03-30 20:01:40 +0000

    fix brokeshit

---
+++ gamemode/items/weapon/weapon_srp_deserteagle.lua
@@ -0,0 +1,17 @@
+ITEM.Name = "IMI Desert Eagle (.357)"
+ITEM.Desc = "The eighties called - they want their gun back. Chambered in .357 Magnum."
+ITEM.Model = "models/weapons/tfa_ins2/w_deagle.mdl"
+ITEM.WeaponClass = "tfa_ins2_deagle"
+ITEM.Weight = 3
+ITEM.SafetyPos = Vector(0,0,0)
+ITEM.SafetyAng = Vector(-12.4, 0, 0)
+ITEM.JamChance = 0.00021
+ITEM.DegradeRate = 0.015
+ITEM.FOV 			= 7;
+ITEM.CamPos 		= Vector( 0, 100, 25 );
+ITEM.LookAt 		= Vector( 1.5, 0, 0 );
+ITEM.SelfRepairCondition = 85
+ITEM.AmmoType = "357mag"
+ITEM.License = "B"
+ITEM.BulkPrice = 136000
+ITEM.Damage = 65
