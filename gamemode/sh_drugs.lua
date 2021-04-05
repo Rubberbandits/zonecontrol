625756acec3a5717c173dfd62cdeb936bfbe6ea6
rusty <dalegribble@riseup.net>
2021-04-05 11:56:42 -0600

    fix oops

--- gamemode/sh_drugs.lua
+++ gamemode/sh_drugs.lua
@@ -10,8 +10,8 @@ function GM:CreateDrugType(class, data)
 	self.DrugEffects[class] = data
 
 	if data.Hooks then
-		for hook,func in next, data.Hooks do
-			hook.Add(hook, "DRUG_"..class, func)
+		for hookName,func in next, data.Hooks do
+			hook.Add(hookName, "DRUG_"..class, func)
 		end
 	end
 end
@@ -35,7 +35,7 @@ end
 function meta:ApplyDrug(class)
 	local data = GAMEMODE:GetDrugData(class)
 	if !data then return end
-	
+
 	if data.Duration then
 		timer.Create("DRUG_"..class, data.Duration, 1, function()
 			self:RemoveDrug(class)
