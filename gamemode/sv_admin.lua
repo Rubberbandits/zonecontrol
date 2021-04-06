cc01426c3488199cd330587ad23a3c0f8d61bcd8
rusty <dalegribble@riseup.net>
2021-04-06 13:51:29 -0600

    rpa_sethealth

--- gamemode/sv_admin.lua
+++ gamemode/sv_admin.lua
@@ -2291,3 +2291,32 @@ local function ChangeItemPrice(ply, args)
 	file.Write("zonecontrol/itemprices.txt", util.TableToJSON(GAMEMODE.ItemPrice, true))
 end
 concommand.AddAdmin("rpa_setitemprice", ChangeItemPrice, true)
+
+local function SetHealth(ply, args)
+	if( #args == 0 ) then
+		
+		ply:Notify(nil, COLOR_ERROR, "Error: no target specified.")
+		return;
+		
+	end
+	
+	local targ = GAMEMODE:FindPlayer( args[1], ply );
+	local health = tonumber(args[2])
+	
+	if !health then
+		ply:Notify(nil, COLOR_ERROR, "Error: invalid health value specified.")
+		return
+	end
+	
+	if( targ and targ:IsValid() ) then
+	
+		targ:SetHealth(health)
+		ply:Notify(nil, COLOR_NOTIF, "You've set %s's health to %d.", targ:RPName(), health)
+		
+	else
+		
+		ply:Notify(nil, COLOR_ERROR, "Error: target not found.")
+		
+	end
+end
+concommand.AddAdmin("rpa_sethealth", SetHealth)
