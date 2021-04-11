f66d9699c1ce1112d1d7aa9f3ed69b9f6e445aaa
grnadiac <81665169+grnadiac@users.noreply.github.com>
2021-04-11 15:50:16 -0500

    Update suit_sunrise.lua

--- gamemode/items/clothes/suit_sunrise.lua
+++ gamemode/items/clothes/suit_sunrise.lua
@@ -12,7 +12,7 @@ ITEM.Bonemerge = "models/tnb/stalker_2019/sunrise.mdl";
 ITEM.RemoveBody = true
 ITEM.AllowGender = true
 ITEM.HelmetBodygroup = {1,1}
-ITEM.DegradationProtection = 10
+ITEM.DegradationProtection = 25
 ITEM.Bodygroups = {
 	{1, 0}
 }
