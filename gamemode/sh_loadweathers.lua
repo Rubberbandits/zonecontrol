0329135f5b8e80d2c02022104d4e38721c406729
rusty <dalegribble@riseup.net>
2021-04-12 23:07:22 -0700

    oops should only run this code sv

--- gamemode/sh_loadweathers.lua
+++ gamemode/sh_loadweathers.lua
@@ -17,14 +17,16 @@ if( #files > 0 ) then
 
 end
 
-hook.Add("InitPostEntity", "STALKER.OverrideStormFox2", function()
-	if StormFox2 then
-		StormFox2.Setting.Set("use_2dskybox", true)
+if SERVER then
+	hook.Add("InitPostEntity", "STALKER.OverrideStormFox2", function()
+		if StormFox2 then
+			StormFox2.Setting.Set("use_2dskybox", true)
 
-		local lastWeather = cookie.Get("sf_lastweather", "Clear")
-		local isClear = lastWeather == "Clear"
-		StormFox2.Weather.Set(lastWeather, isClear and 0 or 1)
+			local lastWeather = cookie.GetString("sf_lastweather", "Clear")
+			local isClear = lastWeather == "Clear"
+			StormFox2.Weather.Set(lastWeather, isClear and 0 or 1)
 
-		RunConsoleCommand("sf_use_2dskybox", "1")
-	end
-end)
+			RunConsoleCommand("sf_use_2dskybox", "1")
+		end
+	end)
+end
