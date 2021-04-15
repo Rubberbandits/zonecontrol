8cc63fc6d77745df7f8733474de2ed2e485c50d2
rusty <dalegribble@riseup.net>
2021-04-14 20:27:49 -0700

    allow zeus lightning at eye trace

--- gamemode/admincmds/rpa_lightning.lua
+++ gamemode/admincmds/rpa_lightning.lua
@@ -1,27 +1,14 @@
 if StormFox2 then
 	local function ZeusStrike( ply, args )
-		
-		if( #args == 0 ) then
-			
-			ply:Notify(nil, COLOR_ERROR, "Error: no target specified.")
-			return;
-			
-		end
-		
-		local targ = GAMEMODE:FindPlayer( args[1], ply );
-		
-		if( targ and targ:IsValid() ) then
-			
-			StormFox2.Thunder.Strike( targ )
-			
-			GAMEMODE:LogAdmin( "[D] " .. ply:Nick() .. " lighnting'd player " .. nick .. ".", ply );
+
+		local targ = args[1] and GAMEMODE:FindPlayer(args[1], ply) or ply:GetEyeTraceNoCursor().HitPos
+
+		StormFox2.Thunder.Strike(targ)
+
+		if isentity(targ) then
+			GAMEMODE:LogAdmin( "[D] " .. ply:Nick() .. " lighnting'd player " .. targ:Nick() .. ".", ply );
 
 			targ:Notify(nil, COLOR_NOTIF, "%s used zeus lightning on you.", ply:Nick())
-			
-		else
-			
-			ply:Notify(nil, COLOR_ERROR, "Error: target not found.")
-			
 		end
 		
 	end
