019ff5824c3e1d8c3071ab3d273c2676e554953a
rusty <dalegribble@riseup.net>
2021-04-18 21:54:58 -0700

    finalize move to rostok, add truck cemetery

--- gamemode/maps/rp_rostok_cmb.lua
+++ gamemode/maps/rp_rostok_cmb.lua
@@ -7,12 +7,14 @@ end
 function GM:MapInitPostEntity()
 	
 	--self:CreateLocationPoint( Vector( -13051.937500, -8850.031250, 118.061867 ), LOCATION_GARBAGE, 200, TRANSITPORT_ROSTOK_GARBAGE );
+	self:CreateLocationPoint( Vector( -3113.026611, -12049.968750, 64.031250 ), LOCATION_TRUCKCEMETERY, 200, TRANSITPORT_ROSTOK_TRUCKCEMETERY );
 	
 end
 
 GM.EnableAreaportals = true;
 
 GM.ConnectMessages[TRANSITPORT_ROSTOK_GARBAGE] = "Continuing down this road takes you to the Garbage, an area of forgotten treasures and heaps of trash.";
+GM.ConnectMessages[TRANSITPORT_ROSTOK_TRUCKCEMETERY] = "Continuing down this road takes you to the Truck Cemetery, where most of vehicles used in the containment of the initial Chernobyl incident ended their lives. Highly dangerous, fortunes are made and lost there."
 
 GM.EntryPortSpawns[TRANSITPORT_GARBAGE_ROSTOK] = {
 	Vector(-13183.833008, -9363.562500, 126.480820),
@@ -24,5 +26,15 @@ GM.EntryPortSpawns[TRANSITPORT_GARBAGE_ROSTOK] = {
 	Vector(-13163.364258, -9581.646484, 127.012955),
 	Vector(-13258.948242, -9542.996094, 127.557472),
 };
+GM.EntryPortSpawns[TRANSITPORT_TRUCKCEMETERY_ROSTOK] = {
+	Vector(-3141.553223, -11305.160156, 64.031250),
+	Vector(-3076.680664, -11302.453125, 64.031250),
+	Vector(-3078.033203, -11247.536133, 64.031250),
+	Vector(-3151.978760, -11248.680664, 64.031250),
+	Vector(-3156.227783, -11169.300781, 64.031250),
+	Vector(-3078.635498, -11164.687500, 64.031250),
+	Vector(-3081.602539, -11089.993164, 64.031250),
+	Vector(-3146.930664, -11089.606445, 64.031250),
+}
 
 GM.CurrentLocation = LOCATION_ROSTOK;
