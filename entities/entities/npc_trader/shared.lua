907780d01311a2fa563b01ef4f94cd6a637bc23e
rusty <dalegribble@riseup.net>
2021-04-21 19:35:22 -0700

    npc vendors completed

--- entities/entities/npc_trader/shared.lua
+++ entities/entities/npc_trader/shared.lua
@@ -9,3 +9,17 @@ ENT.Category		= "Kingston"
 ENT.Spawnable = true
 ENT.AdminOnly = true
 ENT.Vendor = true
+
+function ENT:CanSellItem(item)
+	local item = self.Items[item]
+	if item then
+		return item.SellFactor > 0
+	end
+end
+
+function ENT:CanBuyItem(item)
+	local item = self.Items[item]
+	if item then
+		return item.BuyFactor > 0
+	end
+end
