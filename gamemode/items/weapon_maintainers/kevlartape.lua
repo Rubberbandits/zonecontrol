7d67540d576efbe3b6465ee1c22d7a00f5c36529
Rubberbandits <dalegribble@riseup.net>
2021-04-21 22:22:01 -0600

    more weight fixes

--- gamemode/items/weapon_maintainers/kevlartape.lua
+++ gamemode/items/weapon_maintainers/kevlartape.lua
@@ -2,7 +2,7 @@
 ITEM.Name =  "Kevlar Tape";
 ITEM.Desc =  "Heat and impact resistant aramid fiber strips. Contrary to popular belief, NOT adhesive-backed.";
 ITEM.Model =  "models/kek1ch/armor_repair_fa.mdl";
-ITEM.Weight =  0.5;
+ITEM.Weight =  0.2;
 ITEM.FOV =  13;
 ITEM.CamPos =  Vector( 50, 50, 50 );
 ITEM.LookAt =  Vector( 0, 0, 0 );
