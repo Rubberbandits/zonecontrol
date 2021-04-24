7771b76d2c2d1a551b513deb3921508ff2baccc6
rusty <dalegribble@riseup.net>
2021-04-23 17:17:42 -0700

    fix oops

--- gamemode/items/base/weapon.lua
+++ gamemode/items/base/weapon.lua
@@ -247,7 +247,7 @@ function BASE:CanDrop()
 end
 function BASE:CanUpgrade()
 
-	return !self:GetVar( "Equipped", false ) and item:Owner():IsUpgradeTech() and InStockpileRange(ply)
+	return !self:GetVar( "Equipped", false ) and self:Owner():IsUpgradeTech() and InStockpileRange(ply)
 	
 end
 function BASE:OnGamemodeLoaded()
