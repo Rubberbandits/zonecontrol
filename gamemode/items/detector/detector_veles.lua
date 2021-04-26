62aea931d3ecb49c485110c6986c2843c41670be
Rubberbandits <dalegribble@riseup.net>
2021-04-25 22:23:42 -0600

    made detectors sellable

--- gamemode/items/detector/detector_veles.lua
+++ gamemode/items/detector/detector_veles.lua
@@ -1,15 +1,20 @@
 ITEM.Base = "detector"
-ITEM.Name									=	"Artifact Detector (veles)"
-ITEM.Desc					=	"One of the most advanced artifact detectors you can get a hold of."
-ITEM.Model									=	"models/kali/miscstuff/stalker/detector_veles.mdl"
-ITEM.Size	 								=	1
-ITEM.Amount 								=	1
-ITEM.FOV 			= 8;
+ITEM.Name = "Artifact Detector (veles)"
+ITEM.Desc = "One of the most advanced artifact detectors you can get a hold of."
+ITEM.Model = "models/kali/miscstuff/stalker/detector_veles.mdl"
+ITEM.Size =	1
+ITEM.Amount = 1
+ITEM.FOV = 8;
 ITEM.DetectorType = 3
-ITEM.CamPos 		= Vector( 0.5, 18.75, 60 );
-ITEM.LookAt 		= Vector( 0, 1, 4.83 );
-ITEM.Weight 		= 0.65
-ITEM.BulkPrice		= 200000;
-ITEM.License		= "X";
+ITEM.CamPos = Vector( 0.5, 18.75, 60 );
+ITEM.LookAt = Vector( 0, 1, 4.83 );
+ITEM.Weight = 0.65
+ITEM.BulkPrice = 200000;
+ITEM.License = "X";
 ITEM.Rarity = 5
 ITEM.AllowRandomSpawn = false
+ITEM.IsSellable = true
+
+function ITEM:GetSellPrice()
+    return 40000
+end
