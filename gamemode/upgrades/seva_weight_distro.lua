3407b965d273872698a722e1f9ae3e9baaec733f
Rubberbandits <dalegribble@riseup.net>
2021-05-08 23:22:02 -0700

    suit upgrade test

---
+++ gamemode/upgrades/seva_weight_distro.lua
@@ -0,0 +1,30 @@
+--EFFECT
+UPGRADE.Base = "clothes"
+UPGRADE.Name = "Weight Distribution Vest";
+UPGRADE.Desc = "Special mounts distribute the carried load more evenly across the body.";
+
+UPGRADE.IconPage = 5
+UPGRADE.IconX = 14
+UPGRADE.IconY = 32
+
+UPGRADE.CarryAdd = 5
+
+UPGRADE.PropertiesTooltip = {
+	{
+		IconX = 6,
+		IconY = 3,
+		Text = "+5kg Carrying Capacity.",
+	},
+}
+
+--CLASS
+UPGRADE.Item = {
+    suit_seva = true,
+}
+
+--TIER
+UPGRADE.RequiredItems = {
+	{ "basic_sewkit", 1, false },
+	{ "kevlartape", 1, true },
+    { "thread", 1, true },
+};
