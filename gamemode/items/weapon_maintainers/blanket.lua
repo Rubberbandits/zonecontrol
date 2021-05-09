7fed40e79bae8b697869ea7b63448d3c8afc0c19
Rubberbandits <dalegribble@riseup.net>
2021-05-09 00:21:27 -0700

    model changes

--- gamemode/items/weapon_maintainers/blanket.lua
+++ gamemode/items/weapon_maintainers/blanket.lua
@@ -1,6 +1,6 @@
 ITEM.Name =  "Old Blanket";
 ITEM.Desc =  "An old, ratty blanket. Good for sleeping tight in, but better served ripped apart and used for patchwork.";
-ITEM.Model =  "models/kek1ch/item_sleepbag.mdl"
+ITEM.Model =  "models/kek1ch/bedspread.mdl"
 ITEM.Weight =  0.25;
 ITEM.FOV 			= 21;
 ITEM.CamPos 		= Vector( 73.22, 48.22, 55.36 );
--- gamemode/items/weapon_maintainers/parts_low.lua
+++ gamemode/items/weapon_maintainers/parts_low.lua
@@ -1,6 +1,6 @@
 ITEM.Name = "Low quality parts"
 ITEM.Desc = "A low end set of weapon and suit parts gathered in a small duffel. Not particularly effective for firearms but common throughout the Zone."
-ITEM.Model = "models/kek1ch/armor_repair_pro.mdl"
+ITEM.Model = "models/kek1ch/helmet_repair.mdl"
 ITEM.Weight = 0.75
 ITEM.License = "T"
 ITEM.RaiseCondition = 20
