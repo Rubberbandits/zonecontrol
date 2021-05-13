5f8040f903ef4d76b81e69302ade09b1cd83720c
Rubberbandits <dalegribble@riseup.net>
2021-05-12 22:19:46 -0700

    part prices dropped

--- gamemode/items/parts_weapon.lua
+++ gamemode/items/parts_weapon.lua
@@ -3,6 +3,6 @@ ITEM.Desc = "An assortment of gun parts."
 ITEM.Model = "models/kek1ch/waste_large.mdl"
 ITEM.Weight = 0.5
 ITEM.License = "T"
-ITEM.BulkPrice =  12500;
+ITEM.BulkPrice =  5000;
 ITEM.W = 1
 ITEM.H = 1
