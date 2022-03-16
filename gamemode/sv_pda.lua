474b7f56834bd8de45f2799d1c8c7396fd010c15
rusty <94700317+dimwitsoft@users.noreply.github.com>
2022-03-16 15:42:48 -0600

    use player.GetAllLoaded() in rand pda msg select

--- gamemode/sv_pda.lua
+++ gamemode/sv_pda.lua
@@ -797,7 +797,7 @@ local RandomPDAMessageFuncs = {
 		return table.Random(RandomTalking)
 	end,
 	[3] = function()
-		local randomPlayer = table.Random(player.GetHumans())
+		local randomPlayer = table.Random(player.GetAllLoaded())
 		if !IsValid(randomPlayer) then return end
 
 		local playerName = randomPlayer:CharID() > 0 and randomPlayer:RPName() or false
@@ -818,7 +818,7 @@ local RandomPDAMessageFuncs = {
 		return Format(table.Random(RandomItemRelatedStrings), randomItem.Name)
 	end,
 	[5] = function()
-		local randomPlayer = table.Random(player.GetHumans())
+		local randomPlayer = table.Random(player.GetAllLoaded())
 		if !IsValid(randomPlayer) then return end
 
 		local playerName = randomPlayer:CharID() > 0 and randomPlayer:RPName() or false
@@ -828,7 +828,7 @@ local RandomPDAMessageFuncs = {
 		return Format(table.Random(RandomPlayerRelatedStrings), playerName)
 	end,
 	[6] = function()
-		local randomPlayer = table.Random(player.GetHumans())
+		local randomPlayer = table.Random(player.GetAllLoaded())
 		if !IsValid(randomPlayer) then return end
 
 		local playerName = randomPlayer:CharID() > 0 and randomPlayer:HasCharFlag("X") and randomPlayer:RPName() or false
