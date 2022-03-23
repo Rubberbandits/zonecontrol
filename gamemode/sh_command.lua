07f52fd01ffc067ed331e7362cd6799894f3b5e2
rusty <94700317+dimwitsoft@users.noreply.github.com>
2022-03-23 14:22:30 -0600

    add pda all messages discord integration
    /pda all messages are now sent via webhook to a discord channel
    
    See: #29

--- gamemode/sh_command.lua
+++ gamemode/sh_command.lua
@@ -294,6 +294,17 @@ kingston.command.register("pda", {
 		
 		if args[1] == "all" then
 			netstream.Start(rf, "nAddPDANotif", header, body, 2, 5)
+
+			if GAMEMODE.PDADiscordHook then
+				// fire and forget
+				http.Post(GAMEMODE.ProxySite, {
+					url = GAMEMODE.PDADiscordHook,
+					data = util.TableToJSON({
+						username = header,
+						content = body
+					})
+				})
+			end
 		else
 			netstream.Start(rf, "nAddPDANotif", header, body, 6, 3)
 		end
