3a744c7985677fae857f848cab9abc1fa1c09088
rusty <dalegribble@riseup.net>
2022-03-24 15:41:37 -0600

    add character roster to admin menu

--- gamemode/commands/rpa_flagsroster.lua
+++ gamemode/commands/rpa_flagsroster.lua
@@ -1,18 +1,41 @@
 
 
-kingston.admin.registerCommand("gameflagsroster", {
-	syntax = "<none>",
-	description = "View all characters with flags",
-	arguments = {},
+kingston.admin.registerCommand("gamecharroster", {
+	syntax = "<number page>",
+	description = "View a paginated list of characters",
+	arguments = {ARGTYPE_NUMBER},
 	onRun = function(ply)
-		local function qS(ret)	
-			netstream.Start(ply, "nAFlagsRoster", ret)
-			
-			GAMEMODE:LogSQL("Player " .. ply:Nick() .. " retrieved flags roster.")
-		end
-		
-		local function qF(err) end
-		
-		mysqloo.Query("SELECT RPName, CharFlags FROM cc_chars WHERE CharFlags != ''", qS, qF)
+
 	end,
 })
+
+if SERVER then
+	util.AddNetworkString("zcGetCharacterList")
+
+	local function zcGetCharacterList(len, ply)
+		if !ply:HasPermission("gamecharroster") then return end
+
+		local number = net.ReadUInt(32) - 1
+
+		local function qS(ret)
+			net.Start("zcGetCharacterList")
+				net.WriteUInt(#ret, 32)
+				for _,data in pairs(ret) do
+					net.WriteString(data.SteamID)
+					net.WriteString(data.RPName)
+					net.WriteString(data.CharFlags)
+					net.WriteString(data.Date)
+					net.WriteString(data.LastOnline)
+					net.WriteUInt(data.Money, 32)
+				end
+			net.Send(ply)
+			
+			GAMEMODE:LogSQL("Player " .. ply:Nick() .. " retrieved character roster.")
+		end
+		
+		local function qF(err) print(err) end
+		
+		mysqloo.Query(Format("SELECT SteamID, RPName, CharFlags, Money, Date, LastOnline FROM cc_chars LIMIT %d, 25;", 25 * number), qS, qF)
+	end
+	net.Receive("zcGetCharacterList", zcGetCharacterList)
+end
