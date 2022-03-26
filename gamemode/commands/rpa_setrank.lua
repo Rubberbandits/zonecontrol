82e9068c02ffefbe5f24c29a6ff081f6e36e2f21
rusty <dalegribble@riseup.net>
2022-03-25 18:23:12 -0600

    plysetrank rank argument is optional

--- gamemode/commands/rpa_setrank.lua
+++ gamemode/commands/rpa_setrank.lua
@@ -1,8 +1,10 @@
 kingston.admin.registerCommand("plysetrank", {
 	syntax = "<string target> <string rank>",
 	description = "Set a player's rank",
-	arguments = {bit.bor(ARGTYPE_TARGET, ARGTYPE_STEAMID), ARGTYPE_STRING},
+	arguments = {bit.bor(ARGTYPE_TARGET, ARGTYPE_STEAMID), bit.bor(ARGTYPE_STRING, ARGTYPE_NONE)},
 	onRun = function(ply, target, rank)
+		rank = rank != NULL and rank or "user"
+
 		// add offline steamID support
 		target:SetUserGroup(rank)
 		target:UpdatePlayerField("Rank", rank)
