99fe6bc1f917f64a66966ce917db3887b54b1f50
rusty <dalegribble@riseup.net>
2022-03-27 20:31:45 -0600

    update redrick with example dialog for pirate

--- entities/entities/xp_npc/cl_init.lua
+++ entities/entities/xp_npc/cl_init.lua
@@ -1,18 +1,24 @@
 include( "shared.lua" )
 
 ENT.NPC_CONVERSATION = {
-	opening = {
-		response = "What can I do for you, stalker?",
-		options = {
-			"exit",
-		},
-	},
-	exit = {
-		dialog = "See you around.",
-		callback = function(panel, key)
-			panel:Close()
-		end,
-	},
+    opening = { // this opening is the dialogue shown when you first interact with the NPC
+        response = "What can I do for you, stalker?", // this is what the NPC says
+        options = { // these are your dialogue choices
+            "something_interesting", // from the opening, we can pick either something_interesting or exit
+            "exit",
+        },
+    },
+    something_interesting = { // unique identifier to tell the script what specific dialogue info to use
+        dialog = "See anything interesting around here?", // this is what we are going to say to the NPC
+        response = "It's the same-old-same-old here in the Cordon, a bunch of rookies shitting their pants. What else would it be?" // this is the NPCs response to what we said
+        // if there is no options specified, we go back to options
+    },
+    exit = {
+        dialog = "See you around.",
+        callback = function(panel, key)
+            panel:Close()
+        end,
+    },
 }
 
 net.Receive("ui_help_npc", function(len)
