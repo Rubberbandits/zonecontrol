8a53b6a9b3f1cb6cb4491a82303778a3aa63c2ed
Rubberbandits <dalegribble@riseup.net>
2022-04-20 17:15:52 -0600

    fix namewarn broadcasting to all players

--- gamemode/commands/rpa_namewarn.lua
+++ gamemode/commands/rpa_namewarn.lua
@@ -5,6 +5,6 @@ kingston.admin.registerCommand("charnamewarn", {
 	description = "Warn a player for their character's name",
 	arguments = {ARGTYPE_TARGET},
 	onRun = function(ply, target)
-		netstream.Start( targ, "nWarnName" );
+		netstream.Start( target, "nWarnName" );
 	end,
 })
