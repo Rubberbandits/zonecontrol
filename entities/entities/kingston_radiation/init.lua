8c25bd50c611e96f1a1ee1b8cd60b454cf173dab
rusty <dalegribble@riseup.net>
2022-04-20 13:37:20 -0600

    radiation dosage from amount/hr to amount/min

--- entities/entities/kingston_radiation/init.lua
+++ entities/entities/kingston_radiation/init.lua
@@ -32,7 +32,7 @@ function ENT:Think()
 			local intensity = self:GetSourceIntensity()
 			local calc_amt = math.Clamp(math.Round((intensity * self:GetSourceSize()^2) / dist, 2), 0, intensity) * v:GetRadiationResistance()
 			if calc_amt > 0 then
-				v:ApplyRadiation(math.Round(calc_amt / 3600, 4))
+				v:ApplyRadiation(math.Round(calc_amt / 60, 4))
 			end
 		end
 		
