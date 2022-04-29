b8a124bf56393a0b2f758fe24b5d8629924b7894
Rubberbandits <dalegribble@riseup.net>
2022-04-29 13:36:14 -0600

    f1 menu edits

--- gamemode/cl_help.lua
+++ gamemode/cl_help.lua
@@ -45,37 +45,47 @@ function GM:RefreshHelpMenuContent()
 
 			Adding variables w and y behind language or radio commands will react accordingly, i.e. /engy to yell in English and /rw to whisper over radio.
 			
-			/cmdhelp - Prints a list of available commands that only staff may use. To use these commands in the console, simply type rpa_ before the command.]] },
+			/cmdhelp - Prints a list of available commands that only staff may use. To use these commands in the console, simply type rpa_ before the command.
+			Typing a keyword related to the command you are searching for will list related commands. For example, typing /char will show you every available /char command.]] },
 		{ "Binds", [[F1 - Open help menu.
 		F2 - Open character menu.
-		F3 - Open player menu.
+		F3 - Open inventory menu.
 		F4 - Open admin menu.
 		C - Open context menu.]] },
 		{ "Using a PDA",
 		[[PDA's are an item you must acquire in order to use the Stalker.net service.
-		Register your username if you want to be able to message people privately. If your PDA is annoying you, turn it off.
+		Register your username if you want to be able to message people privately by right-clicking your PDA in the F3 inventory menu and selecting "register".
+		If your PDA is annoying you, you can turn it off by selecting "turn off".
+		Selecting "make primary" on a PDA will make that the PDA you send messages with. 
 
 		/pda all - Send a public message to all PDA users
-		/pda username - Send a private message to a specific PDA username. Use the Contacts tab to find active users.
+		/pda username - Send a private message to a specific PDA username. Use the Contacts tab to find active users. (This feature is currently bugged with usernames that have uppercase letters!)
 
-		When viewing the contents of your PDA, you should find the Journal, Contacts, and Messages tab, as well as a clock.
+		When viewing the contents of your PDA, you should find the Map, Journal, Contacts, and Messages tab, as well as a clock.
+
+		The Map feature is currently under development, and does nothing for now.
 
 		The Journal allows you to take personal notes. Tap the green "New entry" button to create a new entry.
-		Select a title for your entry, then fill out the contents. Handy for writing poems during your roadside picnic.
+		Select a title for your entry, then fill out the contents. Handy for writing poems, documenting important information about your enemies, and saving the location of your stash.
+		Keep in mind that you cannot edit journals once you have saved them, so changes will have to made as a new entry. Also, you are not given a prompt when selecting "Delete", so make sure you don't lose all your work by pressing it on accident.
 
 		The Contacts tab allows you to view all other active PDA users connected to your network.
 
 		The Messages tab will log and date messages you send and receive.
 
-		Extra functions and updates to your PDA system may come overtime! Check back here to learn new functions.]] 
+		Extra functions and updates to your PDA system may come overtime! Check back here to learn about new functions.]] 
 		},
 		{ "Tooltrust", [[By default, you don't have tooltrust. You have phystrust, and you have proptrust. Phys- and proptrust give you a physgun and the ability to spawn props respectively. Tooltrust gives you a toolgun.
 		
-		Basic tooltrust gives you some common simple tools, a slightly increased prop limit, and slightly increased prop spawn permissions. To get this, ask an admin.
+		Basic tooltrust gives you some common simple tools, a slightly increased prop limit, and slightly increased prop spawn permissions. To get this, ask a staff member.
 		
-		Advanced tooltrust gives you most tools, an increased prop limit, and increased prop spawn permissions. Advanced tooltrust users' props are solid, whereas basic and non tooltrusted props are no-collided. To get this, ask an admin.
+		Advanced tooltrust gives you most tools, an increased prop limit, and increased prop spawn permissions. Advanced tooltrust users' props are solid, whereas basic and non tooltrusted props are no-collided. To get this, ask a staff member.
 		
-		Admins can take away phys and proptrust if you abuse the privillege - you can get it back through Discord.]] 
+		Staff can take away phys and proptrust if you abuse the privillege - you can get it back through the forums.]] 
+		},
+		{ "Stockpiles", [[Stockpiles are an extended inventory system outside of your regular F3 inventory menu. Stockpiles are mandatory for Technicians and Traders to conduct business and use their features. Stockpiles are also recommended for factions and PBGs to store extra gear. Not all players may be granted a Stockpile; you must have a need and a right to have one.
+
+		Regular players cannot spawn the Stockpile entity themselves, they must ask a staff member to spawn one for them. ]]
 		},
 		{ "Flags", 
 			[[ZoneControl uses a flag system. Most flags are automatic - you don't need to run a command to access them.
@@ -83,7 +93,6 @@ function GM:RefreshHelpMenuContent()
 			Character Flags
 			T - Technician
 			X - Trader
-			P - PAC3 Access (Donator or Screenshot Contest Winner Only)
 		
 			Trader Flags
 			A - Ammunition
