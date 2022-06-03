129c72b80759a51b47731e5ba62db2b8e69a6620
rusty <dalegribble@riseup.net>
2022-06-03 16:33:04 -0600

    fix rpa_togglewatched

--- gamemode/commands/rpa_togglewatched.lua
+++ gamemode/commands/rpa_togglewatched.lua
@@ -1,7 +1,7 @@
 kingston.admin.registerCommand("togglewatched", {
 	syntax = "<string target>",
 	description = "Toggle whether or not a target is watched by the big brother system",
-	arguments = {},
+	arguments = {bit.bor(ARGTYPE_TARGET, ARGTYPE_STEAMID)},
 	onRun = function(ply, target)
 		target:SetWatched(!target:Watched())
 		target:UpdatePlayerField( "Watched", target:Watched() and "1" or "0" )
