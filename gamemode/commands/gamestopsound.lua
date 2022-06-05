1b151e135fa0eaab740e1e7e919c5e47ae9970f9
rusty <dalegribble@riseup.net>
2022-06-04 18:08:49 -0600

    fix gamestopsound

--- gamemode/commands/gamestopsound.lua
+++ gamemode/commands/gamestopsound.lua
@@ -1,10 +1,10 @@
 kingston.admin.registerCommand("gamestopurl", {
-	syntax = "<number index [optional]>",
-	description = "Stop playing sounds by index or all currently playing URLs",
-	arguments = {bit.bor(ARGTYPE_NUMBER, ARGTYPE_NONE)},
+	syntax = "<number index>",
+	description = "Stop playing sounds by index",
+	arguments = {ARGTYPE_NUMBER},
 	onRun = function(ply, index)
 		net.Start("zcStopSound")
-			net.WriteUInt(url, 8)
+			net.WriteUInt(index, 8)
 		net.Broadcast()
 
 		ply:Notify(nil, COLOR_NOTIF, "Sound stopped for all players.")
