fb42f4ce6c0146b07f1290036f3247d20bc03563
Rubberbandits <dalegribble@riseup.net>
2022-06-05 23:46:24 -0600

    price fix

--- gamemode/items/medical/smallmedkit.lua
+++ gamemode/items/medical/smallmedkit.lua
@@ -1,14 +1,14 @@
-ITEM.Name =  "Gauze Bandages";
-ITEM.Desc =  "A roll of gauze bandages. Used for emergency first aid.";
-ITEM.Model =  "models/stalker/item/medical/bandage.mdl";
+ITEM.Name =  "Gauze Bandages"
+ITEM.Desc =  "A roll of gauze bandages. Used for emergency first aid."
+ITEM.Model =  "models/stalker/item/medical/bandage.mdl"
 ITEM.ConsumeText = "You unravel the roll of gauze and wrap your wound tight, securing it with a knot. It's enough for now."
 ITEM.HealAmount = 15
-ITEM.Weight =  0.2;
-ITEM.FOV =  9;
-ITEM.CamPos =  Vector( 50, 50, 50 );
-ITEM.LookAt =  Vector( 0, 0, 5.02 );
-ITEM.BulkPrice =  225;
-ITEM.License =  "D";
+ITEM.Weight =  0.2
+ITEM.FOV =  9
+ITEM.CamPos =  Vector( 50, 50, 50 )
+ITEM.LookAt =  Vector( 0, 0, 5.02 )
+ITEM.BulkPrice =  250
+ITEM.License =  "D"
 ITEM.ItemSubmaterials = {
 	{ 0 , "models/stalkertextures/item/medkit1" }
 }
