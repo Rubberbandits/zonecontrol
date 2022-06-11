54b99a5c143b73cf29942a434decd65733eae77f
Rubberbandits <dalegribble@riseup.net>
2022-06-10 21:09:06 -0600

    simple effects for cocaine

--- gamemode/items/consumable/cocaine.lua
+++ gamemode/items/consumable/cocaine.lua
@@ -8,3 +8,28 @@ ITEM.LookAt =  Vector( 0, 0, 0 )
 ITEM.BulkPrice =  3750
 ITEM.ConsumeText = "You pop open the small container of white powder, tugging the grain of snow up into one of your nostrils. You feel a powerful headrush in no time, amped and pumped. You're ready to take on the world!"
 ITEM.UseText = "snort"
+ITEM.DrugType = "COCAINE"
+GM:CreateDrugType("COCAINE", {
+	Duration = 600,
+	Hooks = {
+		GetPlayerSpeeds = function(ply, w, r, j, c)
+			if ply:HasDrug("COCAINE") then
+				return w, r * 1.3, j, c
+			end
+		end,
+		PlayerDrugApplied = function(ply, drug)
+			if drug == "COCAINE" then
+				hook.Run("SpeedThink", ply)
+			end
+		end,
+		PlayerDrugRemoved = function(ply, drug)
+			if drug == "COCAINE" then
+				hook.Run("SpeedThink", ply)
+			end
+        GetInventoryMaxWeight = function(ply, weight)
+            if ply:HasDrug("COCAINE") then
+                return weight + 20
+            end               
+		end,
+	},
+})
