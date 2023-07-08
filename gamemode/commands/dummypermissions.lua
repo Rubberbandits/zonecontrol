d19a5f12074724d0ac1e63d2ffda2b0a74781549
Rubberbandits <dalegribble@riseup.net>
2023-07-08 18:32:11 +0100

    create dummy commands for non command based permissions

---
+++ gamemode/commands/dummypermissions.lua
@@ -0,0 +1,27 @@
+kingston.admin.registerCommand("noclip", {
+	syntax = "",
+	description = "",
+	arguments = {},
+	onRun = function() end,
+})
+
+kingston.admin.registerCommand("spawneffect", {
+	syntax = "",
+	description = "",
+	arguments = {},
+	onRun = function() end,
+})
+
+kingston.admin.registerCommand("spawnragdoll", {
+	syntax = "",
+	description = "",
+	arguments = {},
+	onRun = function() end,
+})
+
+kingston.admin.registerCommand("spawnsent", {
+	syntax = "",
+	description = "",
+	arguments = {},
+	onRun = function() end,
+})
